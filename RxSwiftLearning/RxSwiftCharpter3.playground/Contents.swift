e82d4eeef710394fc5ee9c666d09d586af0552c6
mengxiangjian <mengxiangjian@le.com>
2018-11-23 13:37:49 +0800

    Rxswift book charpter 3: Subject

--- RxSwiftLearning/RxSwiftCharpter3.playground/Contents.swift
+++ RxSwiftLearning/RxSwiftCharpter3.playground/Contents.swift
@@ -76,3 +76,174 @@ example(of: "BehaviorSubject") {
     
     print("subscriber2 also receive the error event")
 }
+
+/*
+ ReplaySubject: replay lattest buffer size events to subscriber.
+ Be aware of buffer size, too large buffer size could make memory warning.
+ */
+
+let replaySubject = ReplaySubject<String>.create(bufferSize: 2)
+
+example(of: "ReplaySubject") {
+    replaySubject.onNext("1")
+    replaySubject.onNext("2")
+    
+    replaySubject.subscribe {
+        print(label: "1)", event: $0)
+    }.disposed(by: bag)
+    
+    replaySubject.onNext("3")
+    
+    replaySubject.subscribe {
+        print(label: "2)", event: $0)
+    }.disposed(by: bag)
+    
+    replaySubject.onCompleted()
+    
+    replaySubject.subscribe {
+        print(label: "3)", event: $0)
+    }.disposed(by: bag)
+    
+    print("Every subscriber receives the completed event. Event buffer is not include the completed event.")
+    
+    replaySubject.dispose()
+    
+    replaySubject.subscribe {
+        print(label: "4)", event: $0)
+    }.disposed(by: bag)
+    print("Replay Subject can be disposed manually.")
+}
+
+let variable = Variable<String>("initial value")
+
+example(of: "Variable") {
+    variable.asObservable().subscribe {
+        print(label: "1)", event: $0)
+    }.disposed(by: bag)
+    
+    variable.value = "1"
+    
+    variable.asObservable().subscribe {
+        print(label: "2)", event: $0)
+    }.disposed(by: bag)
+    
+    variable.value = "2"
+    
+    print("current variable's value is \(variable.value)")
+    
+//    variable.asObservable().onError
+//    variable.asObservable().onCompleted()
+    print("Variable can't emit error/completed event. Waitting for dealloc!")
+}
+
+/*
+ Challenge 1: blackjack card dealer.
+ */
+
+example(of: "Challenge 1") {
+
+    let disposeBag = DisposeBag()
+
+    let dealtHand = PublishSubject<[(String, Int)]>()
+
+    func deal(_ cardCount: UInt) {
+        var deck = cards
+        var cardsRemaining: UInt32 = 52
+        var hand = [(String, Int)]()
+
+        for _ in 0..<cardCount {
+            let randomIndex = Int(arc4random_uniform(cardsRemaining))
+            hand.append(deck[randomIndex])
+            deck.remove(at: randomIndex)
+            cardsRemaining -= 1
+        }
+
+        // Add code to update dealtHand here
+        let totalPoint = points(for: hand)
+        if totalPoint > 21 {
+            dealtHand.onError(HandError.busted)
+        } else {
+            dealtHand.onNext(hand)
+        }
+    }
+
+    // Add subscription to dealtHand here
+    dealtHand.subscribe(onNext: {
+        let handCards = cardString(for: $0)
+        let totalPoints = points(for: $0)
+        print("We stay in the game. Hand cards are \(handCards), total point is \(totalPoints)")
+    }, onError: {
+        print($0)
+    }).disposed(by: disposeBag)
+
+    deal(3)
+}
+
+example(of: "Challenge 2") {
+    
+    enum UserSession {
+        
+        case loggedIn, loggedOut
+    }
+    
+    enum LoginError: Error {
+        
+        case invalidCredentials
+    }
+    
+    let disposeBag = DisposeBag()
+    
+    // Create userSession Variable of type UserSession with initial value of .loggedOut
+    let variable = Variable<UserSession>(.loggedOut)
+    
+    
+    // Subscribe to receive next events from userSession
+    variable.asObservable().subscribe(onNext: {
+        print("login state is: \($0)")
+    }).disposed(by: disposeBag)
+    
+    
+    func logInWith(username: String, password: String, completion: (Error?) -> Void) {
+        guard username == "johnny@appleseed.com",
+            password == "appleseed"
+            else {
+                completion(LoginError.invalidCredentials)
+                return
+        }
+        
+        // Update userSession
+        variable.value = .loggedIn
+        completion(nil)
+    }
+    
+    func logOut() {
+        // Update userSession
+        variable.value = .loggedOut
+    }
+    
+    func performActionRequiringLoggedInUser(_ action: () -> Void) {
+        // Ensure that userSession is loggedIn and then execute action()
+        if variable.value == .loggedIn {
+            action()
+        }
+    }
+    
+    for i in 1...2 {
+        let password = i % 2 == 0 ? "appleseed" : "password"
+        
+        logInWith(username: "johnny@appleseed.com", password: password) { error in
+            guard error == nil else {
+                print(error!)
+                return
+            }
+            
+            print("User logged in.")
+        }
+        
+        performActionRequiringLoggedInUser {
+            print("Successfully did something only a logged in user can do.")
+        }
+    }
+}
+
+
